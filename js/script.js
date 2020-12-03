0504c9a32a7d4befb31798ccd05e1129d1ce8687
Blayth <58435589+Blayth@users.noreply.github.com>
2020-12-03 18:10:30 +0100

    modifiche home page 02/12/20

--- js/script.js
+++ js/script.js
@@ -1,20 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
-    let invia = document.getElementById("invia");
-    let iscrizione = document.getElementById("iscrizione");
-    let nome = document.getElementById("nome").value;
-    let cognome = document.getElementById("cognome").value;
-    let eta = document.getElementById("eta").value;
-    let errore = document.getElementById("errore");
 
     invia.addEventListener("click", function(){
-        console.log(nome.length);
-    if (nome.length == 0){
-        errore.style = "display = none";
-    }
-    else if (nome.length > 1){
-        errore.style = "display = none";
-    }
-
+      
     })
 
 
