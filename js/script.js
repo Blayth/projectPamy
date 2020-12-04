606095baa3cce0655c8962f290e36b1fee370ca3
Blayth <58435589+Blayth@users.noreply.github.com>
2020-12-04 13:57:27 +0100

    Modifiche 03/12/20

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-    invia.addEventListener("click", function(){
-      
+    let open = document.getElementById("but-1");
+    let close = document.getElementsByClassName("btn btn-outline-secondary");
+    console.log(close)
+    
+    open.addEventListener("click", function(e){
+        e.preventDefault();
+        document.getElementById("article-home-news").style = "display: block"
     })
 
+    for(var i = 0; i < close.length; i++) {
+        (function(index) {
+            close[index].addEventListener("click", function() {
+                document.getElementById("article-home-news").style = "display: none"
+           })
+        })(i);
+    }
 
 });
