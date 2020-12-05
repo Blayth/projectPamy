c970ea801e3a989e30d5db610c197ec50c19ddd6
Blayth <58435589+Blayth@users.noreply.github.com>
2020-12-05 12:46:25 +0100

    Modifice js

--- js/script.js
+++ js/script.js
@@ -1,20 +1,34 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-    let open = document.getElementById("but-1");
+    let open = document.getElementsByClassName("my_box_button");
     let close = document.getElementsByClassName("btn btn-outline-secondary");
-    console.log(close)
-    
-    open.addEventListener("click", function(e){
-        e.preventDefault();
-        document.getElementById("article-home-news").style = "display: block"
-    })
+    let article = document.getElementsByClassName("article-home-news");
 
-    for(var i = 0; i < close.length; i++) {
-        (function(index) {
-            close[index].addEventListener("click", function() {
-                document.getElementById("article-home-news").style = "display: none"
-           })
-        })(i);
+
+    function find_element(element1, element2, number){
+        let len = 0;
+        for (let i = 0; i < number.length; i++) {
+            len++
+            console.log(len)
+        }
+
+        for(i = 0; i < element1.length; i++){
+            (function(index){
+                element1[index].addEventListener("click", function(){    
+                document.getElementById("article-home-news").style = "display: block";
+                })
+            })(i);
+        }
+
+        for(i = 0; i < element2.length; i++){
+            (function(index){
+                element2[index].addEventListener("click", function(){    
+                document.getElementById("article-home-news").style = "display: none";
+                })
+            })(i);
+        }
+        
     }
 
-});
+    find_element(open, close, open)
+})
